85bf7dfce5ad43cbe94cb46f91b51f9915dbc035
laylalayla100 <awesomeljh@icloud.com>
2023-07-14 10:28:38 -0400

    Initial Commit

---
+++ lol/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  lol
+//
+//  Created by layla hunter on 7/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "heart")
+                .foregroundColor(Color.red)
+                .imageScale(.large)
+                
+            Text("Layla Jazz Hunter")
+        }
+        .padding()
+    
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lol/lolApp.swift
@@ -0,0 +1,17 @@
+//
+//  lolApp.swift
+//  lol
+//
+//  Created by layla hunter on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct lolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
